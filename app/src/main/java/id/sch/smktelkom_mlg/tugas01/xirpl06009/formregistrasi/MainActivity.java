a3751fe89d0b3831ff5934b03172e7e9ef715514
Dhea Armalivia <daffyderr@gmail.com>
2016-09-11 17:39:02 +0700

    lastt commit 6th commit Pembenaran script yang error, menampilkan data inputan dan menampilkan waktu untuk kegiatan pembelajaran

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl06009/formregistrasi/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl06009/formregistrasi/MainActivity.java
@@ -5,21 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     EditText eTuser, eTln, eTemail, eTaddress, eTnumber;
     Button bSub;
     RadioButton rBB, rBM, rBE;
     RadioGroup rGLevel;
-    TextView Res;
+    TextView Res, sub, lev, dur;
     Spinner spCity;
     CheckBox cBHtml, cBCss, cBJava, cBVB, cBC, cBDel;
+    int nSub;
+    private boolean valid;
 
 
     @Override
@@ -33,6 +36,20 @@ public class MainActivity extends AppCompatActivity {
         eTaddress = (EditText) findViewById(R.id.editTextAddress);
         eTnumber = (EditText) findViewById(R.id.editTextNumber);
 
+        cBHtml = (CheckBox) findViewById(R.id.checkBoxHTML);
+        cBCss = (CheckBox) findViewById(R.id.checkBoxCSS);
+        cBJava = (CheckBox) findViewById(R.id.checkBoxJ);
+        cBVB = (CheckBox) findViewById(R.id.checkBoxVB);
+        cBC = (CheckBox) findViewById(R.id.checkBoxC);
+        cBDel = (CheckBox) findViewById(R.id.checkBoxD);
+
+        cBHtml.setOnCheckedChangeListener(this);
+        cBCss.setOnCheckedChangeListener(this);
+        cBJava.setOnCheckedChangeListener(this);
+        cBC.setOnCheckedChangeListener(this);
+        cBVB.setOnCheckedChangeListener(this);
+        cBDel.setOnCheckedChangeListener(this);
+
         rBB = (RadioButton) findViewById(R.id.radioButtonB);
         rBM = (RadioButton) findViewById(R.id.radioButtonM);
         rBE = (RadioButton) findViewById(R.id.radioButtonE);
@@ -43,6 +60,9 @@ public class MainActivity extends AppCompatActivity {
         bSub = (Button) findViewById(R.id.buttonSub);
 
         Res = (TextView) findViewById(R.id.Result);
+        sub = (TextView) findViewById(R.id.subject);
+        lev = (TextView) findViewById(R.id.lvl);
+        dur = (TextView) findViewById(R.id.wkt);
 
         bSub.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,21 +72,78 @@ public class MainActivity extends AppCompatActivity {
                 String ln = eTln.getText().toString();
                 String email = eTemail.getText().toString();
                 String address = eTaddress.getText().toString();
-                int number = Integer.parseInt(eTnumber.getText().toString());
+                String number = eTnumber.getText().toString();
                 String kota = spCity.getSelectedItem().toString();
 
-                Res.setText("\nUsername    : " + uname +
-                        "\nNama Lengkap: " + ln +
-                        "\nEmail       : " + email +
-                        "\nAlamat      : " + address +
-                        "\nNo Telepon  : " + number +
-                        "\nAsal Kota   : " + kota);
+                if (uname.isEmpty()) {
+                    eTuser.setError("Username belum diisi");
+                    valid = false;
+                } else if (ln.isEmpty()) {
+                    eTln.setError("Nama Lengkap belum diisi");
+                    valid = false;
+                } else if (email.isEmpty()) {
+                    eTemail.setError("Email belum diisi");
+                    valid = false;
+                } else if (address.isEmpty()) {
+                    eTaddress.setError("Alamat masih kosong");
+                    valid = false;
+                } else if (number.isEmpty()) {
+                    eTnumber.setError("Nomor Telepon belum diisi");
+                    valid = false;
+                } else {
+                    Res.setText("\nUsername    : " + uname +
+                            "\nNama Lengkap: " + ln +
+                            "\nEmail       : " + email +
+                            "\nAlamat      : " + address +
+                            "\nNo Telepon  : " + number +
+                            "\nAsal Kota   : " + kota);
+                }
 
-                //  doProccess();
+                String hasil = "Materi yang anda pilih:\n";
+                int startlen = hasil.length();
+                if (cBHtml.isChecked()) hasil += cBHtml.getText() + "\n";
+                if (cBCss.isChecked()) hasil += cBCss.getText() + "\n";
+                if (cBJava.isChecked()) hasil += cBJava.getText() + "\n";
+                if (cBVB.isChecked()) hasil += cBVB.getText() + "\n";
+                if (cBC.isChecked()) hasil += cBC.getText() + "\n";
+                if (cBDel.isChecked()) hasil += cBDel.getText() + "\n";
+
+                if (hasil.length() == startlen) hasil += "Anda belum memilih materi";
+
+                sub.setText(hasil);
+
+
+                String Rb = null;
+
+                if (rBB.isChecked()) {
+                    Rb = rBB.getText().toString();
+                } else if (rBM.isChecked()) {
+                    Rb = rBM.getText().toString();
+                } else if (rBE.isChecked()) {
+                    Rb = rBE.getText().toString();
+                }
+
+                if (Rb == null) {
+                    lev.setText("Anda belum memilih level");
+                } else {
+                    lev.setText("Level yang Anda pilih untuk kegiatan ini : " +
+                            Rb);
+                }
+
+
+
             }
         });
 
     }
 
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) nSub += 2;
+        else nSub -= 2;
+
+        dur.setText("Waktu untuk pembelajaran " + nSub + " minggu");
+
+    }
 }
 
