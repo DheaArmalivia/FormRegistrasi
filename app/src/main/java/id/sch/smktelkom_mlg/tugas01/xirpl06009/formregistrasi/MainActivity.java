91ce7ab0f7da34f040d228eb45c2f18b40855578
Dhea Armalivia <daffyderr@gmail.com>
2016-09-11 10:59:20 +0700

    2nd commit pemberian nama variabel dan deklarasi variabel pada MainActivity.java

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl06009/formregistrasi/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl06009/formregistrasi/MainActivity.java
@@ -2,8 +2,18 @@ package id.sch.smktelkom_mlg.tugas01.xirpl06009.formregistrasi;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText eTuser, eTln, eTemail, eTaddress, eTnumber;
+    Button bSub, bRes;
+    RadioButton rBB, rBM, rBE;
+    CheckBox cBHtml, cBCss, cBJava, cBVB, cBC, cBDel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
