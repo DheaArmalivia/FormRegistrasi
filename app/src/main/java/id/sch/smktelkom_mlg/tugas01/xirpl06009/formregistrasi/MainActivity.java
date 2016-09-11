9d359414beb3155b245ecaf6fb74d66c05fc9327
Dhea Armalivia <daffyderr@gmail.com>
2016-09-11 12:26:16 +0700

    3rd commit pemberian fungsi pada button Submit (1) validasi editText

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl06009/formregistrasi/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl06009/formregistrasi/MainActivity.java
@@ -2,16 +2,23 @@ package id.sch.smktelkom_mlg.tugas01.xirpl06009.formregistrasi;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText eTuser, eTln, eTemail, eTaddress, eTnumber;
     Button bSub, bRes;
     RadioButton rBB, rBM, rBE;
+    RadioGroup rGLevel;
+    TextView Res;
+    Spinner spCity;
     CheckBox cBHtml, cBCss, cBJava, cBVB, cBC, cBDel;
 
 
@@ -19,5 +26,77 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eTuser = (EditText) findViewById(R.id.editTextUname);
+        eTln = (EditText) findViewById(R.id.editTextLname);
+        eTemail = (EditText) findViewById(R.id.editTextEmail);
+        eTaddress = (EditText) findViewById(R.id.editTextAddress);
+        eTnumber = (EditText) findViewById(R.id.editTextNumber);
+
+        rBB = (RadioButton) findViewById(R.id.radioButtonB);
+        rBM = (RadioButton) findViewById(R.id.radioButtonM);
+        rBE = (RadioButton) findViewById(R.id.radioButtonE);
+
+        rGLevel = (RadioGroup) findViewById(R.id.level);
+        spCity = (Spinner) findViewById(R.id.spinnerKota);
+
+        bSub = (Button) findViewById(R.id.buttonSub);
+        bRes = (Button) findViewById(R.id.buttonRes);
+
+        Res = (TextView) findViewById(R.id.Result);
+
+        bSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doProccess();
+            }
+        });
+
     }
+
+    private void doProccess() {
+
+        if (isValid()) {
+            Res.setText("Data Anda sudah terkirim");
+        }
+
+    }
+
+    private boolean isValid() {
+
+        boolean valid = true;
+
+        String uN = eTuser.getText().toString();
+        String lN = eTln.getText().toString();
+        String el = eTemail.getText().toString();
+        String ad = eTaddress.getText().toString();
+        String nt = eTnumber.getText().toString();
+        String hasil = null;
+
+        if (uN.isEmpty()) {
+            eTuser.setError("Username belum diisi");
+            valid = false;
+        } else if (lN.isEmpty()) {
+            eTln.setError("Nama Lengkap belum diisi");
+            valid = false;
+        } else if (el.isEmpty()) {
+            eTemail.setError("Email belum diisi");
+            valid = false;
+        } else if (ad.isEmpty()) {
+            eTaddress.setError("Alamat masih kosong");
+            valid = false;
+        } else if (nt.isEmpty()) {
+            eTnumber.setError("Nomor Telepon belum diisi");
+        } else {
+            eTuser.setError(null);
+            eTln.setError(null);
+            eTaddress.setError(null);
+            eTnumber.setError(null);
+            eTemail.setError(null);
+        }
+
+        return valid;
+
+    }
+
 }
